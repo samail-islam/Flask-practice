12a1a077251edb5d412d53c2bc7cc46434d6d77a
Samail Islam <samailislam17@gmail.com>
2025-05-08 22:29:24 +0600

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,19 @@ import requests
 
 app = Flask(__name__)
 
+@app.route('/')
+def homepage():
+    return "<h2>This is the homepage</h2>"
+
+@app.route('/users')
+def raw_code():
+    username = request.args.get('user')
+    if not username:
+        return "error: no username provided"
+    github_url = f"https://github.com/{username}"
+    response = requests.get(github_url)
+    return response.text[:5000]
+
 @app.route('/api/github-stats')
 def github_stats():
     username = request.args.get('username')
@@ -12,4 +25,4 @@ def github_stats():
     github_api_url = f"https://api.github.com/users/{username}"
     response = requests.get(github_api_url)
     
-    return response.text[:500]
+    return response.text[:5000]
