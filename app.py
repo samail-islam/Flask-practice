fabe4b93b695226fabe7f698b419985acce1d0d2
Samail Islam <samailislam17@gmail.com>
2025-05-09 22:38:33 +0600

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, Response
+from flask import Flask, request, Response, render_template
 import requests
 
 app = Flask(__name__)
@@ -15,8 +15,40 @@ def raw_code():
     else:
         github_url = f"https://github.com/{username}"
         response = requests.get(github_url)
-        text = response.text[:800]
-        return f"<p style=color:blue>This is {text}</p>"
+        text = response.text[:80000]
+        return text
+
+@app.route('/svgtest')
+def svg():
+    name = request.args.get('username')
+    if not name:
+        pass
+    login = request.args.get('login')
+    if not login:
+        pass
+    public_repos = request.args.get('prepos')
+    if not pubic_repos:
+        pass
+    following = request.args.get('following')
+    if not following:
+        pass
+    followers = request.args.get('followers')
+    if not followers:
+        pass
+    avatar_url = request.args.get('avturl')
+    if not avatar_url:
+        pass
+    return Response(
+        render_template("card.svg", 
+                        name,
+                        login,
+                        public_repos,
+                        followers,
+                        following,
+                        avatar_url,
+        mimetype="image/svg+xml"
+    )
+    
 
 @app.route('/api/github-stats')
 def github_stats():
